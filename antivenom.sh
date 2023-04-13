3de23c0665292efa55029d684757d7f364e6ab72
oak-and-lion <oakandlionsociety@gmail.com>
2023-04-13 11:10:53 -0400

    update scripts

---
+++ antivenom.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo you need to specify the folder to clear out!
+  echo valid values are:
+  echo                        jes == JES2\\OUTPUT\\
+  echo                  converted == converted\\
+  exit 1
+fi
+
+if [ "$1" != "jes" ] && [ "$1" != "converted" ]; then
+  echo you need to specify the folder to clear out!
+  echo valid values are:
+  echo                        jes == JES2\\OUTPUT\\
+  echo                  converted == converted\\
+  exit 1
+fi
+
+if [ "$1" == "jes" ]; then
+    echo clear jes
+    rm -f JES2/OUTPUT/*
+fi
+
+if [ "$1" == "converted" ]; then
+    echo clear converted
+    rm -f converted/*.py
+fi
